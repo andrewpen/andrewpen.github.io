65bda3ed62b48d2700d93c0f0dd02695221216df
Andrew Pendleton <andrew.d.pendleton@gmail.com>
2017-02-03 13:55:49 -0600

    Committing updated files

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,1 @@
-$(window).scroll(function() {
-	$("#splash div").css({
-	'opacity' : 1-(($(this).scrollTop())/250)
-	});
-});
-
-$(document).ready(function() {
-	$('a').smoothScroll({offset: -125, afterScroll: function() {
-		alert('we made it!');
-	}});
-});
+function test(){return 5}
