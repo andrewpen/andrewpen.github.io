b99622c89e265a329192585f92c9dcad8875fc61
Andrew Pendleton <andrew.d.pendleton@gmail.com>
2021-03-23 19:51:51 -0500

    Updating dashboard files.

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,74 @@
-$(document).ready(function(){
-    //json data for handlebar template
-    var data = {templateEngine:"Handlebar",
-                italicText:"<i>This text should be in italics</i>",
-                group_id:"Admin"
-    };
-    var dashLinks = {
+//======================================================================
+//
+//  Body links
+//
+//======================================================================  
+var dashLinks = [
+    {
         link_group: "Admin",
         link_label: "Home",
         link_url: "https://www.verizon.com",
         link_image: "",
         link_target: "_blank"
-    };
-    var dashGroups =  {
-        group_id: "Admin",
-        group_url: "/admin"
+    },
+    {
+        link_group: "Admin",
+        link_label: "Shop",
+        link_url: "https://www.verizon.com/shop",
+        link_image: "",
+        link_target: "_blank"
+    },
+    {
+        link_group: "Admin",
+        link_label: "Deals",
+        link_url: "https://www.verizon.com/deals",
+        link_image: "",
+        link_target: "_blank"
     }
-    
-    //handlebar template html
-    var template = $("#dashTemplateLinks").html();
+]
+
+
+
+
+
+$(document).ready(function(){
+//======================================================================
+//
+//  Navigation links
+//
+//======================================================================
+    var dashGroups =  [
+        {
+            group_id: "Dash",
+            group_url: "/dash"
+        },
+        {
+            group_id: "Admin",
+            group_url: "/admin"
+        },
+        {
+            group_id:"AEM",
+            group_url:"/aem"
+        }
+    ]
     var navTemplate = $("#dashNavLinks").html();
-    
-    //handlebar's compile method returns special function which can be used to get final html
-    var compiledCode = Handlebars.compile(template);
     var compiledNavCode = Handlebars.compile(navTemplate);
-    
-    //json data is passed top compiled code and result will be html
-    var result = compiledCode(dashGroups);
     var navResult = compiledNavCode(dashGroups);
-    
-    //compiled html can be rendered in document
-    $("#content").html(result);
     $("#nav_content").html(navResult);
+//======================================================================
+//======================================================================
 
 
 
-  });
+});
+
+  function test(group_id){
+      console.log(group_id);
+    var bodyTemplate = $("#dashBodyLinks").html();
+    var compiledBodyCode = Handlebars.compile(bodyTemplate);
+    var bodyResult = compiledBodyCode(dashLinks);
+    $("#body_content").html(bodyResult);
+  };
   
   
   
