ccaa78253ac2a02d3debadd72fe6940a45c61737
Andrew Pendleton <andrew.d.pendleton@gmail.com>
2021-03-23 21:35:06 -0500

    Updates to dashboard files.

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,119 @@
 //======================================================================
 //
-//  Body links
-//
-//======================================================================  
-var dashLinks = [
-    {
-        link_group: "Admin",
-        link_label: "Home",
-        link_url: "https://www.verizon.com",
-        link_image: "",
-        link_target: "_blank"
-    },
-    {
-        link_group: "Admin",
-        link_label: "Shop",
-        link_url: "https://www.verizon.com/shop",
-        link_image: "",
-        link_target: "_blank"
-    },
-    {
-        link_group: "Admin",
-        link_label: "Deals",
-        link_url: "https://www.verizon.com/deals",
-        link_image: "",
-        link_target: "_blank"
-    }
-]
-
-
-
-
-
-$(document).ready(function(){
-//======================================================================
-//
 //  Navigation links
 //
 //======================================================================
-    var dashGroups =  [
+var dashGroups =  [
+    {
+        group_id: "Dash",
+        group_label: "dash"
+    },
+    {
+        group_id: "Admin",
+        group_label: "admin"
+    },
+    {
+        group_id:"AEM",
+        group_label:"aem"
+    }
+]
+//======================================================================
+//
+//  Body links
+//
+//======================================================================  
+var dashLinks = {
+    dash: [
         {
-            group_id: "Dash",
-            group_url: "/dash"
+            link_label: "Dash link 1",
+            link_url: "https://www.verizon.com",
+            link_image: "",
+            link_target: "_blank" 
         },
         {
-            group_id: "Admin",
-            group_url: "/admin"
+            link_label: "Dash link 2",
+            link_url: "https://www.verizon.com",
+            link_image: "",
+            link_target: "_blank" 
         },
         {
-            group_id:"AEM",
-            group_url:"/aem"
+            link_label: "Dash link 3",
+            link_url: "https://www.verizon.com",
+            link_image: "",
+            link_target: "_blank" 
+        },
+        {
+            link_label: "Dash link 4",
+            link_url: "https://www.verizon.com",
+            link_image: "",
+            link_target: "_blank" 
+        }
+    ],
+    admin: [
+        {
+            link_label: "Home",
+            link_url: "https://www.verizon.com",
+            link_image: "",
+            link_target: "_blank"
+        },
+        {
+            link_label: "Shop",
+            link_url: "https://www.verizon.com/shop",
+            link_image: "",
+            link_target: "_blank"
+        },
+        {
+            link_label: "Deals",
+            link_url: "https://www.verizon.com/deals",
+            link_image: "",
+            link_target: "_blank"
+        }
+    ],
+    aem: [
+        {
+            link_label: "AEM link 1",
+            link_url: "https://www.adobe.com",
+            link_image: "",
+            link_target: "_blank"
+        },
+        {
+            link_label: "AEM link 2",
+            link_url: "https://www.adobe.com",
+            link_image: "",
+            link_target: "_blank"
         }
     ]
+}
+
+$(document).ready(function(){
+// Populate left column navigation
     var navTemplate = $("#dashNavLinks").html();
     var compiledNavCode = Handlebars.compile(navTemplate);
     var navResult = compiledNavCode(dashGroups);
     $("#nav_content").html(navResult);
-//======================================================================
-//======================================================================
-
-
-
 });
 
-  function test(group_id){
-      console.log(group_id);
+// Populate body navigation based on nav click
+function popGroupLinks(group_label){
+    console.log(group_label);
     var bodyTemplate = $("#dashBodyLinks").html();
     var compiledBodyCode = Handlebars.compile(bodyTemplate);
-    var bodyResult = compiledBodyCode(dashLinks);
+    
+    switch(group_label) {
+        case "dash":
+            var bodyResult = compiledBodyCode(dashLinks.dash);
+            break;
+        case "admin":
+            var bodyResult = compiledBodyCode(dashLinks.admin);
+            break;
+        case "aem":
+            var bodyResult = compiledBodyCode(dashLinks.aem);
+            break;
+        default:
+            var bodyResult = compiledBodyCode(dashLinks.dash);
+            break;
+      }
+    group_label = toUpperCase(group_label);
+    $("h1").html(group_label + " Links");
     $("#body_content").html(bodyResult);
-  };
-  
-  
-  
+};
