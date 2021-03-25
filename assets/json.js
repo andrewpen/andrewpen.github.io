efb44ec9037a27ab4c036618fb9a07b8a24bcd99
Andrew Pendleton <andrew.d.pendleton@gmail.com>
2021-03-25 13:38:48 -0500

    Adding link groups to the json file.

--- assets/json.js
+++ assets/json.js
@@ -81,3 +81,120 @@ var dashLinks = {
         }
     ]
 }
+
+
+
+
+var p1_Links = {
+    group_1: [
+        {
+            link_label: "Page 1, Group 1, Link 1",
+            link_url: "https://www.google.com/save",
+            link_image: "https://www.google.com/s2/favicons?domain=google.com",
+            link_target: "_blank"
+        },
+        {
+            link_label: "Page 1, Group 1, Link 2",
+            link_url: "https://www.verizon.com",
+            link_image: "https://www.google.com/s2/favicons?domain=verizon.com",
+            link_target: "_blank" 
+        },
+        {
+            link_label: "Page 1, Group 1, Link 3",
+            link_url: "https://www.google.com",
+            link_image: "https://www.google.com/s2/favicons?domain=google.com",
+            link_target: "_blank" 
+        }
+    ],
+    group_2: [
+        {
+            link_label: "Page 1, Group 2, Link 1",
+            link_url: "https://www.verizon.com",
+            link_image: "https://www.google.com/s2/favicons?domain=andrewpen.github.io",
+            link_target: "_blank"
+        },
+        {
+            link_label: "Page 1, Group 2, Link 2",
+            link_url: "https://www.verizon.com/shop",
+            link_image: "https://www.google.com/s2/favicons?domain=gitlab.com",
+            link_target: "_blank"
+        },
+        {
+            link_label: "Page 1, Group 2, Link 3",
+            link_url: "https://www.verizon.com/deals",
+            link_image: "https://www.google.com/s2/favicons?domain=github.com",
+            link_target: "_blank"
+        }
+    ],
+    group_3: [
+        {
+            link_label: "Page 1, Group 3, Link 1",
+            link_url: "https://www.amazon.com",
+            link_image: "https://www.google.com/s2/favicons?domain=amazon.com",
+            link_target: "_blank"
+        },
+        {
+            link_label: "Page 1, Group 3, Link 2",
+            link_url: "https://www.adobe.com",
+            link_image: "https://www.google.com/s2/favicons?domain=adobe.com",
+            link_target: "_blank"
+        }
+    ]
+}
+
+var p2_Links = {
+    group_1: [
+        {
+            link_label: "Page 2, Group 1, Link 1",
+            link_url: "https://www.google.com/save",
+            link_image: "https://www.google.com/s2/favicons?domain=google.com",
+            link_target: "_blank"
+        },
+        {
+            link_label: "Page 2, Group 1, Link 2",
+            link_url: "https://www.verizon.com",
+            link_image: "https://www.google.com/s2/favicons?domain=verizon.com",
+            link_target: "_blank" 
+        },
+        {
+            link_label: "Page 2, Group 1, Link 3",
+            link_url: "https://www.google.com",
+            link_image: "https://www.google.com/s2/favicons?domain=google.com",
+            link_target: "_blank" 
+        }
+    ],
+    group_2: [
+        {
+            link_label: "Page 2, Group 2, Link 1",
+            link_url: "https://www.verizon.com",
+            link_image: "https://www.google.com/s2/favicons?domain=andrewpen.github.io",
+            link_target: "_blank"
+        },
+        {
+            link_label: "Page 2, Group 2, Link 2",
+            link_url: "https://www.verizon.com/shop",
+            link_image: "https://www.google.com/s2/favicons?domain=gitlab.com",
+            link_target: "_blank"
+        },
+        {
+            link_label: "Page 2, Group 2, Link 3",
+            link_url: "https://www.verizon.com/deals",
+            link_image: "https://www.google.com/s2/favicons?domain=github.com",
+            link_target: "_blank"
+        }
+    ],
+    group_3: [
+        {
+            link_label: "Page 2, Group 3, Link 1",
+            link_url: "https://www.amazon.com",
+            link_image: "https://www.google.com/s2/favicons?domain=amazon.com",
+            link_target: "_blank"
+        },
+        {
+            link_label: "Page 2, Group 3, Link 2",
+            link_url: "https://www.adobe.com",
+            link_image: "https://www.google.com/s2/favicons?domain=adobe.com",
+            link_target: "_blank"
+        }
+    ]
+}
