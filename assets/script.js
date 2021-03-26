b9bf2892a35eb38e58e4adaf1e4f25cf57712345
Andrew Pendleton <andrew.d.pendleton@gmail.com>
2021-03-25 22:27:01 -0500

    Late night updates.

--- assets/script.js
+++ assets/script.js
@@ -29,3 +29,17 @@ function popGroupLinks(group_label){
     $("h2").html(group_label + " Links");
     $("#body_content").html(bodyResult);
 };
+
+// Populate body navigation based on nav click version 2!
+function popGroupLinks2(group_label){
+    var bodyTemplate = $("#dashBodyLinks").html();
+    var compiledBodyCode = Handlebars.compile(bodyTemplate);   
+    switch(group_label) {
+        case "dash": var bodyResult = compiledBodyCode(dashLinks.dash); break;
+        case "admin": var bodyResult = compiledBodyCode(dashLinks.admin); break;
+        case "aem": var bodyResult = compiledBodyCode(dashLinks.aem); break;
+        default: var bodyResult = compiledBodyCode(dashLinks.dash); break;
+      }    
+    $("h2").html(group_label + " Links");
+    $("#body_content").html(bodyResult);
+};
