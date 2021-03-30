75a176e8347d68c822b55f985cc73f2679ada0aa
Andrew Pendleton <andrew.d.pendleton@gmail.com>
2021-03-30 12:40:39 -0500

    Removing unused files.

--- nextjs/next-app/util/config.js
+++
@@ -1,7 +0,0 @@
-let config = {};
-config.db = {};
-// Create properties on the config.db object for the host and 
-// database names
-config.db.host = ‘localhost:27017’;
-config.db.name = ‘account’;
-module.exports = config;
